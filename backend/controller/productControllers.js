72356b11fcf0d363685c664f95786a33aea864a7
unknown <thetai2605@gmail.com>
2021-10-21 17:13:29 +0700

    sua run config + taikhoandb.txt

---
+++ backend/controller/productControllers.js
@@ -0,0 +1,13 @@
+const Product = require("../models/Product");
+
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+module.exports = { getProducts };
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
-import express from "express";
-
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+const productRoutes = require("./routes/productRoutes");
 const app = express();
 
+//connect to DB
+connectDB()
+
 app.get("/", (req, res) => {
-  res.send("Server is ready");
+  res.json({ message: "API running..." });
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server at http://localhost:${port}`);
+// router
+app.use("/api/products", productRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
