9c9eea7a92af67d5f8c6b1d7a2993ddea0963ca3
Nguyễn Văn Hồng <70307363+sitpro993@users.noreply.github.com>
2021-11-08 16:24:52 +0700

    chuyen sang nextjs

--- backend/routes/productRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getProducts } = require("../controller/productControllers");
-
-router.get("/", getProducts);
-
-module.exports = router;
