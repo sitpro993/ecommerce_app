3f7fa5835ae93675e91621bdd90e4a9e2e959f17
Nguyễn Văn Hồng <70307363+sitpro993@users.noreply.github.com>
2021-12-30 13:48:39 +0700

    update deploy

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     domains: ["res.cloudinary.com"],
   },
   env: {
-    BASE_URL: "http://localhost:3000",
+    BASE_URL: "https://ecommerce-app-lake.vercel.app/",
     MONGODB_URL:
       "mongodb+srv://web:12345@cluster0.amsca.mongodb.net/beeyou?retryWrites=true&w=majority",
     ACCESS_TOKEN_SECRET: "}Jr%Sn:Np8^{$X~c?*Wb/8_)>]&L{JH[V?w}dZ.zHhyd9f8b!",
