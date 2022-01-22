f57c4d6f7dbfc8b5d9f6af15bcf224b752113a33
Nguyễn Văn Hồng <70307363+sitpro993@users.noreply.github.com>
2022-01-22 20:38:19 +0700

    update

--- pages/account/index.js
+++ pages/account/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import Image from "next/image";
 import Head from "next/head";
 import ParallaxScrolling from "../../components/HomeComponent/ParallaxScrolling";
