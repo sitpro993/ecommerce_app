759f55b428829da1ce6129d664a2a3af7097b6f9
phamthiminhthu <minhthutb111@gmail.com>
2022-01-24 01:18:52 +0700

    add list order

--- components/MyOrderItemHeader.js
+++ components/MyOrderItemHeader.js
@@ -1,11 +1,20 @@
 import React from "react";
+import moment from 'moment';
 
-export default function MyOrderItemHeader({ order }) {
+
+export default function MyOrderItemHeader({ order, index }) {
+  const convertDateShip = (createdDate) => {
+    return moment(createdDate).format('DD/MM/YYYY HH:mm:ss');
+  }
   return (
-    <div className="row" style={{ width: "100%" }}>
-      <div className="col-3">{order._id}</div>
-      <div className="col-3">{order.totalPrice}</div>
-      <div className="col-3">{order.createdAt}</div>
+    <div className="row information-order" style={{ width: "100%" }}>
+      <div className="col-1">{index}</div>
+      <div className="col-2 col-status">{order.isPaid && order.isDelivere ? <span>Hoàn thành</span> : <span>Đang giao dịch</span>}</div>
+      <div className="col-1">{order.isPaid ? <i class="fas fa-circle"></i> : <i class="far fa-circle"></i>}</div>
+      <div className="col-1">{order.isDelivered ? <i class="fas fa-circle"></i> : <i class="far fa-circle"></i>}</div>
+      <div className="col-2">{order.totalPrice} VND</div>
+      <div className="col-2">{convertDateShip(order.createdAt)}</div>
+      <div className="col-2">{convertDateShip(order.updatedAt)}</div>
     </div>
   );
 }
--- pages/don-hang-cua-toi.js
+++ pages/don-hang-cua-toi.js
@@ -25,12 +25,13 @@ export default function MyOrders() {
     const getOrders = async () => {
       const data = await getData("users/orders", auth.token);
       setOrders(data);
+
     };
     if (auth.token) {
       getOrders();
     }
   }, [auth.token]);
-
+  console.log(orders);
   return (
     <>
       <Head>
@@ -39,16 +40,28 @@ export default function MyOrders() {
       </Head>
       <ParallaxScrolling></ParallaxScrolling>
 
-      <section className="wrapper" style={{ marginBottom: "30px" }}>
+      <section className="wrapper" style={{ marginBottom: "50px" }}>
+        <h3 className="text-center mt-5 mb-5">Thông tin đơn hàng của tôi</h3>
         <Accordion>
+          <Accordion.Item>
+            <div className="row col-information-order">
+              <div className="col-1">Đơn hàng</div>
+              <div className="col-2">Trạng thái</div>
+              <div className="col-1 col-left">Thanh toán</div>
+              <div className="col-1 col-left">Giao hàng</div>
+              <div className="col-2 col-left">Tổng thanh toán</div>
+              <div className="col-2 col-left">Ngày tạo đơn hàng</div>
+              <div className="col-2 col-left">Ngày cập nhật cuối</div>
+            </div>
+          </Accordion.Item>
           {orders.length > 0 ? (
             orders.map((order, index) => (
               <Accordion.Item eventKey={index} key={index}>
                 <Accordion.Header>
-                  <MyOrderItemHeader order={order} />
+                  <MyOrderItemHeader order={order} index={index + 1} />
                 </Accordion.Header>
                 <Accordion.Body>
-                  {/* <MyOrderItemBody order={order} /> */}
+                  <MyOrderItemBody order={order} />
                 </Accordion.Body>
               </Accordion.Item>
             ))
